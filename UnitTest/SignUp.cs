35fa71f5a66b87ebdcb70af6cc286a1fb7d9b280
KethanKumar13 <kethankumar136@gmail.com>
2024-10-14 06:56:32 +0530

    Test empty signup field

--- UnitTest/SignUp.cs
+++ UnitTest/SignUp.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using HomeServices_Testing.Utilities;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace HomeServices_Testing.UnitTest
 {
@@ -37,6 +38,44 @@ namespace HomeServices_Testing.UnitTest
             IWebElement register = driver.FindElement(By.XPath("//button[normalize-space()='Register']"));
             register.Click();
             Thread.Sleep(2000);
+        }
+
+        [Test]
+        public void TestSignUpWithEmptyFields()
+        {
+            Thread.Sleep(2000);
+            IWebElement signup = driver.FindElement(By.XPath("//a[normalize-space()='Sign up']"));
+            signup.Click();
+            Thread.Sleep(2000);
+
+            IWebElement name = driver.FindElement(By.XPath("//input[@id='Display Name']"));
+            name.Click();
+            Thread.Sleep(2000);
+            name.SendKeys("");
+
+            IWebElement email = driver.FindElement(By.XPath("//input[@id='Email Address']"));
+            email.Click();
+            Thread.Sleep(2000);
+            email.SendKeys("");
+
+            IWebElement password = driver.FindElement(By.XPath("//input[@id='Password']"));
+            password.Click();
+            password.SendKeys("");
+            Thread.Sleep(2000);
+
+            IWebElement register = driver.FindElement(By.XPath("//button[normalize-space()='Register']"));
+            register.Click();
+            Thread.Sleep(2000);
+
+
+            IWebElement nameError = driver.FindElement(By.XPath("//span[normalize-space()='Display Name is required']"));
+            IWebElement emailError = driver.FindElement(By.XPath("//span[normalize-space()='Email Address is required']"));
+            IWebElement passwordError = driver.FindElement(By.XPath("//span[normalize-space()='Password is required']"));
+
+            // Assert that error messages are displayed
+            Assert.IsTrue(nameError.Displayed, "Error message for Display Name is not displayed.");
+            Assert.IsTrue(emailError.Displayed, "Error message for Email Address is not displayed.");
+            Assert.IsTrue(passwordError.Displayed, "Error message for Password is not displayed.");
         }
 
     }
